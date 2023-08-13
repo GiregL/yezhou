0d172174e590e7336e4789a6cff372e399733b08
Lezenn <lezenn@localhost.localdomain>
2023-08-13 15:21:17 +0200

    Init

---
+++ src/main/java/fr/lezenn/yezhou/YezhouApplication.java
@@ -0,0 +1,13 @@
+package fr.lezenn.yezhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YezhouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YezhouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/lezenn/yezhou/YezhouApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lezenn.yezhou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YezhouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
